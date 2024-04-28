bcc410e7616dd647442e3602384c5c0e8e7d2b5d
Mayer-04 <andres.20.chaves@gmail.com>
2024-04-27 22:58:54 -0500

    Cambiando migraciones

--- prisma/migrations/20240428031835_init/migration.sql
+++ prisma/migrations/20240428031835_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "Users" (
     "lastname" VARCHAR(30) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "age" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
